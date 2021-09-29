6862a5a790a7a5ad1058caf19c78070bea66fbc7
pruscius <andi_frank19@hotmail.com>
2021-09-29 18:35:08 -0300

    axios to api and parcial country model

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,37 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, FLOAT } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    // id: {
+    //   type: DataTypes.
+    // },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    region: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      // allowNull: false
+    },
+    subregion: {
+      type: DataTypes.STRING
+    },
+    area: {
+      type: DataTypes.FLOAT
+    },
+    population: {
+      type: DataTypes.FLOAT
+    }
+
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,53 @@
+const axios = require('axios');
+const { Country } = require('../db.js')
+// const { Country } = require('../models/Country.js');
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+
+const getData = async () => {
+    try {
+        const data = await axios.get('https://restcountries.com/v2/all');
+        // .data devuelve la data del get
+        const json = data.data;
+        for (var i = 0; i < json.length; i++) {
+            const [country, created] = await Country.findOrCreate({
+            where: {
+                name: json[i].name
+            },
+            defaults: {
+                name: json[i].name,
+                flag: json[i].flag,
+                region: json[i].region,
+                capital: json[i].capital,
+                subregion: json[i].subregion,
+                area: json[i].area,
+                population: json[i].population
+            }
+            }) 
+        }
+    } catch(e) {
+        console.log(e);
+    }
+}
+// invoco la función para obtener los datos de la API.
+getData();
+
+
+// ID (Código de 3 letras) *
+// Nombre *
+// Imagen de la bandera *
+// Continente *
+// Capital *
+// Subregión
+// Área
+// Población
+
+// const json = await getData();
+// console.log(json);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
