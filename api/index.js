13e37be70fff0a2fab0de5ac9b8ca88e5c56409f
pruscius <andi_frank19@hotmail.com>
2022-01-05 18:56:31 -0300

    changed port

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+let port = process.env.PORT || 3001;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
