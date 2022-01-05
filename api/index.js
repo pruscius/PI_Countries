09651f3a54ca2c1e45f2dbb897c2c4067c49d17e
pruscius <andi_frank19@hotmail.com>
2022-01-05 19:04:46 -0300

    CHANGED PORT

--- api/index.js
+++ api/index.js
@@ -21,11 +21,10 @@ require('dotenv').config()
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-let port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
