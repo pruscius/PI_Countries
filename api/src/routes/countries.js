60db055b4c19e48a5dda3b58b19a2c097e1d36e4
pruscius <andi_frank19@hotmail.com>
2022-01-05 21:22:58 -0300

    getData invoked

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -11,69 +11,72 @@ const router = Router();
 //             res.json(response)
 //         }).catch(e=>res.json(e));
 // })
-async function dataBase(req, res, next) {
-    try {
-        const allCountries = await Country.findAll(); //busco los paises en la DB
 
-        //si no hay nada
-        if (!allCountries.length) {
-            //hago la peticion a la API, recorro todo lo q me trae y voy creando paises con las condiciones requeridas
-            let countriesDB = await axios.get('https://restcountries.com/v2/all');
-            countriesDB = countriesDB.data.map(c => {
-                return Country.create(
 
-                    {
-                        id: c.alpha3Code,
-                        name: c.name,
-                        flag: c.flag,
-                        region: c.region,
-                        capital: c.capital,
-                        subregion: c.subregion,
-                        area: c.area,
-                        population: c.population
-                    })
-            })
-            //una vez que se cumplieron todas las prmesas de la DB
-            //que continue
-            Promise.all(countriesDB)
-                .then(response => next())
-        } else { //si ya estan los datos en la DB que continue
-            return next();
-        }
-    } catch (err) {
-        next(err)
-    }
-}
-
-// const getData = async () => {
+// async function dataBase(req, res, next) {
 //     try {
-//         const data = await axios.get('https://restcountries.com/v2/all');
-//         // .data devuelve la data del get
-//         const json = data.data;
-//         for (var i = 0; i < json.length; i++) {
-//             const [country, created] = await Country.findOrCreate({
-//                 where: {
-//                     name: json[i].name
-//                 },
-//                 defaults: {
-//                     id: json[i].alpha3Code,
-//                     name: json[i].name,
-//                     flag: json[i].flag,
-//                     region: json[i].region,
-//                     capital: json[i].capital,
-//                     subregion: json[i].subregion,
-//                     area: json[i].area,
-//                     population: json[i].population
-//                 }
+//         const allCountries = await Country.findAll(); //busco los paises en la DB
+
+//         //si no hay nada
+//         if (!allCountries.length) {
+//             //hago la peticion a la API, recorro todo lo q me trae y voy creando paises con las condiciones requeridas
+//             let countriesDB = await axios.get('https://restcountries.com/v2/all');
+//             countriesDB = countriesDB.data.map(c => {
+//                 return Country.create(
+
+//                     {
+//                         id: c.alpha3Code,
+//                         name: c.name,
+//                         flag: c.flag,
+//                         region: c.region,
+//                         capital: c.capital,
+//                         subregion: c.subregion,
+//                         area: c.area,
+//                         population: c.population
+//                     })
 //             })
+//             //una vez que se cumplieron todas las prmesas de la DB
+//             //que continue
+//             Promise.all(countriesDB)
+//                 .then(response => next())
+//         } else { //si ya estan los datos en la DB que continue
+//             return next();
 //         }
-//     } catch (e) {
-//         console.log(e);
+//     } catch (err) {
+//         next(err)
 //     }
 // }
 
+const getData = async () => {
+    try {
+        const data = await axios.get('https://restcountries.com/v2/all');
+        // .data devuelve la data del get
+        const json = data.data;
+        for (var i = 0; i < json.length; i++) {
+            const [country, created] = await Country.findOrCreate({
+                where: {
+                    name: json[i].name
+                },
+                defaults: {
+                    id: json[i].alpha3Code,
+                    name: json[i].name,
+                    flag: json[i].flag,
+                    region: json[i].region,
+                    capital: json[i].capital,
+                    subregion: json[i].subregion,
+                    area: json[i].area,
+                    population: json[i].population
+                }
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-router.get('/', async (req, res) => {
+getData();
+
+router.get('/', dataBase, async (req, res) => {
     const { name, order, filter } = req.query;
     console.log(order, filter)
     if (order && filter) {
