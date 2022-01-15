f8eb22d4352f39db3f08e996e1034bac00ad4570
pruscius <andi_frank19@hotmail.com>
2022-01-14 22:02:01 -0300

    finished landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,13 +5,19 @@ import { Link } from 'react-router-dom';
 export default function Landing () {
     return (
         <div className={styles.landing}>
+            <div className={styles.divName}>
+                <h1 className={styles.name}>Countrivities</h1>
+            </div>
             <div className={styles.buttonDiv}>
-                <Link to="/home">
-                    <button className={styles.button}>ENTER</button>
+                <Link className={styles.link} to="/home">
+                    <button className={styles.button}>DISCOVER</button>
                 </Link>
             </div>
-            <div className={styles.divText}>
-                <h1 className={styles.text}>COUNTRIVITIES</h1>
+            <div className={styles.divSlogan}>
+                <h1 className={styles.slogan}>YOU PICK THE PLACE
+                    <br/>
+                    WE SET THE STAGE
+                </h1>
             </div>
         </div>
 
