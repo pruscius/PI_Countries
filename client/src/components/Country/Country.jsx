353cd9ff5f1d66a7e0685b5621f70878578e6070
pruscius <andi_frank19@hotmail.com>
2022-01-16 00:46:08 -0300

    fix country card

--- client/src/components/Country/Country.jsx
+++ client/src/components/Country/Country.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 
 export default function Country({ id, name, flag, region, population }) {
     return (
-        <div style={{backgroundImage: `url(${flag})`}} className={styles.container}>
+        // <div style={{backgroundImage: `url(${flag})`}} className={styles.container}>
+        <div className={styles.container}>
+            <img src={flag} className={styles.flag} alt="Not found"/>
             <Link to={`/countries/${id}`} className={styles.link}>
                 <div key={id} className={styles.card}>
                     <h3 className={styles.name}>{name}</h3>
-                    {/* <img src={flag} className={styles.flag} alt="Not found"/> */}
                     <h4 className={styles.continent}>{region}</h4>
                 </div>
             </Link>
