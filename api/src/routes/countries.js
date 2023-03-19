8b7a38dccb822559286fe724d856fc148cfa08f7
pruscius <andi_frank19@hotmail.com>
2023-03-19 13:20:35 -0300

    repopulate DB

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -12,7 +12,41 @@ const router = Router();
 //         }).catch(e=>res.json(e));
 // })
 
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
+const getData = async (next) => {
+    try {
+        const allCountries = await Country.findAll(); //busco los paises en la DB
+        if (!allCountries.length) {
+            const data = await axios.get('https://restcountries.com/v2/all');
+            // .data devuelve la data del get
+            const json = data.data;
+            for (var i = 0; i < json.length; i++) {
+                const [country, created] = await Country.findOrCreate({
+                    where: {
+                        name: json[i].name
+                    },
+                    defaults: {
+                        id: json[i].alpha3Code,
+                        name: json[i].name,
+                        flag: json[i].flag,
+                        region: json[i].region,
+                        capital: json[i].capital,
+                        subregion: json[i].subregion,
+                        area: json[i].area,
+                        population: json[i].population
+                    }
+                })
+            }
+            return next();
+        } else {
+            return next();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+router.get('/', getData, async (req, res) => {
     const { name, order, filter } = req.query;
     console.log(order, filter)
     if (order && filter) {
